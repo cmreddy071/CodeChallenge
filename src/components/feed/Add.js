bc0f0d7bd8e1b8d19e7b89762194060dd4031ed9
Chinna Mohana Venkata Gurrala <ramireddy.gurrala@eagles.oc.edu>
2019-03-14 10:54:16 -0400

    final code

---
+++ src/components/feed/Add.js
@@ -0,0 +1,77 @@
+import React from "react";
+
+class Add extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      feedItem: {
+        url: "",
+        title: ""
+      }
+    };
+  }
+
+  handleInputChange = event => {
+    const eve = { ...event };
+    this.setState(prevState => ({
+      feedItem: {
+        ...prevState.feedItem,
+        [eve.target.name]: [eve.target.value]
+      }
+    }));
+  };
+
+  handleAdd = event => {
+    event.preventDefault();
+    this.props.onNewPost(this.state.feedItem);
+  };
+
+  render() {
+    return (
+      <form style={styles.addForm}>
+        <input
+          type="text"
+          name="url"
+          placeholder="Enter Image URL"
+          style={styles.input}
+          value={this.state.feedItem.url}
+          onChange={this.handleInputChange}
+        />
+        <input
+          type="text"
+          name="title"
+          placeholder="Enter Title"
+          style={styles.input}
+          value={this.state.feedItem.title}
+          onChange={this.handleInputChange}
+        />
+        <button style={styles.button} onClick={this.handleAdd}>
+          New Post
+        </button>
+      </form>
+    );
+  }
+}
+
+const styles = {
+  addForm: {
+    padding: "8px",
+    margin: "16px 0",
+    background: "#f5f5f5"
+  },
+  input: {
+    width: "100%",
+    padding: "6px 10px",
+    margin: "12px 0",
+    boxSizing: "border-box"
+  },
+  button: {
+    background: "#E91E63",
+    color: "#fff",
+    border: "1px solid",
+    padding: "8px 10px",
+    cursor: "pointer"
+  }
+};
+
+export default Add;
